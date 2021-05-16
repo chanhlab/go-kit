5f6c848d92d7cdeb45f867c67c51b5055e4470cf
Duong <duongnv129@gmail.com>
2021-05-16 10:55:59 +0700

    update test for mysql

--- database/mysql/mysql_test.go
+++ database/mysql/mysql_test.go
@@ -9,7 +9,6 @@ import (
 )
 
 func TestGetConnectionShouldReturnNil(t *testing.T) {
-	t.Skip()
 	logger.NewDefault()
 	db := GetConnection("127.0.0.1", "db_service", "root", "123456", 10, 10)
 	assert.NotNil(t, db)
