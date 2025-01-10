9dae63153ce0344e4023af813def70c7776938f8
Duong Nguyen <duongnv129@gmail.com>
2025-01-10 21:32:05 +0700

    fix lint

--- rest/middleware/tracing.go
+++ rest/middleware/tracing.go
@@ -13,7 +13,7 @@ var (
 )
 
 // init Initializes constant part of request ID
-// nolint: gochecknoinits
+//nolint: gochecknoinits
 func init() {
 	grpcGatewayTag = &opentracing.Tag{Key: string(ext.Component), Value: "grpc-gateway"}
 }
